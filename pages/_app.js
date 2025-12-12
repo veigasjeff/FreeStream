300497a918281b70a54ffc94bf675fe446ba247e
verdsouza <110382877+veigasjeff@users.noreply.github.com>
2025-12-12 15:33:47 +0530

    UPDATED ADS

--- pages/_app.js
+++ pages/_app.js
@@ -25,7 +25,7 @@ export default function App({ Component, pageProps }) {
     // <script>(function(s){s.dataset.zone='10297166',s.src='https://groleegni.net/vignette.min.js'})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))</script>
 
     // <script>(function(s){s.dataset.zone='10297164',s.src='https://nap5k.com/tag.min.js'})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))</script>
-    Load ads after page is fully loaded
+    // Load ads after page is fully loaded
     if (document.readyState === 'complete') {
       loadAdScripts()
     } else {
@@ -67,55 +67,3 @@ export default function App({ Component, pageProps }) {
   );
 }
 
-
-
-
-
-// import '../styles/globals.css';
-// import Header from '../components/Header';
-// import Footer from '../components/Footer';
-// import { useEffect } from 'react'
-// import { useRouter } from 'next/router'
-
-// export default function App({ Component, pageProps }) {
-//   const router = useRouter()
-
-//   // Track page views for Google Analytics
-//   useEffect(() => {
-//     const handleRouteChange = (url) => {
-//       if (window.gtag) {
-//         window.gtag('config', 'G-9TY4WQ89PC', {
-//           page_title: document.title,
-//           page_location: url
-//         })
-//       }
-//     }
-
-//     router.events.on('routeChangeComplete', handleRouteChange)
-//     return () => {
-//       router.events.off('routeChangeComplete', handleRouteChange)
-//     }
-//   }, [router.events])
-
-//   // Load Google Analytics if not already loaded
-//   useEffect(() => {
-//     if (!window.gtag) {
-//       window.dataLayer = window.dataLayer || [];
-//       function gtag(){dataLayer.push(arguments);}
-//       gtag('js', new Date());
-//       gtag('config', 'G-RTHH33WQWQ');
-//     }
-//   }, [])
-
-//   return (
-//     <>
-//       <div className="min-h-screen flex flex-col">
-//         <Header />
-//         <main className="flex-grow">
-//           <Component {...pageProps} />
-//         </main>
-//         <Footer />
-//       </div>
-//     </>
-//   );
-// }
