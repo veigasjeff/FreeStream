43b3566dd371eb90dd3dd2b429d5b66671393430
verdsouza <110382877+veigasjeff@users.noreply.github.com>
2025-12-16 14:00:28 +0530

    re check

--- pages/_app.js
+++ pages/_app.js
@@ -18,7 +18,7 @@ export default function App({ Component, pageProps }) {
 
       // Load second ad script
       const adScript2 = document.createElement("script");
-      adScript2.innerHTML = `(function(s){s.dataset.zone='10297166',s.src='https://groleegni.net/vignette.min.js'})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))`;
+      adScript2.innerHTML = `(function(s){s.dataset.zone='10333130',s.src='https://groleegni.net/vignette.min.js'})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))`;
       document.head.appendChild(adScript2);
 
          // Load third ad script
@@ -26,6 +26,8 @@ export default function App({ Component, pageProps }) {
       adScript3.innerHTML = `(function(s){s.dataset.zone='10333088',s.src='https://al5sm.com/tag.min.js'})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))`;
       document.head.appendChild(adScript3);
     };
+
+    // <script>(function(s){s.dataset.zone='10333130',s.src='https://groleegni.net/vignette.min.js'})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))</script>
 
     // <script>(function(s){s.dataset.zone='10333088',s.src='https://al5sm.com/tag.min.js'})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))</script>
 
