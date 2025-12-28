ed83d67c5cd93cf6c1e8360ff8246d963b796b9f
verdsouza <110382877+veigasjeff@users.noreply.github.com>
2025-12-28 21:49:35 +0530

    revised code final updated

--- generate-movies.js
+++ generate-movies.js
@@ -113,7 +113,7 @@ function generateMoviePage(movie) {
     <meta property="og:title" content="Watch ${movie.title} Online Free">
     <meta property="og:description" content="${movie.description.replace(/"/g, '&quot;')}">
     <meta property="og:image" content="${posterUrl}">
-    <meta property="og:url" content="https://freestream.vercel.app/movies/${movie.id}/">
+    <meta property="og:url" content="https://freestreaming.vercel.app/movies/${movie.id}/">
     <meta property="og:type" content="video.movie">
     
     <!-- Twitter -->
@@ -1056,7 +1056,7 @@ function generateMoviePage(movie) {
     <button class="theme-toggle" id="themeToggle">
         <i class="fas fa-moon"></i>
     </button>
-    <!-- Ad Scripts -->
+    <!-- Ad Scripts 
     <script>(function(s){s.dataset.zone='10297164',s.src='https://nap5k.com/tag.min.js'})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))</script>
     <script>(function(s){s.dataset.zone='10333131',s.src='https://groleegni.net/vignette.min.js'})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))</script>
 
@@ -1070,7 +1070,7 @@ function generateMoviePage(movie) {
 
   gtag('config', 'G-RTHH33WQWQ');
 </script>
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">-->
 
     <script>
         // Movie data
@@ -1505,7 +1505,7 @@ movies.forEach((movie, index) => {
 
 // Generate sitemap.xml
 console.log('\n📄 Generating sitemap.xml...');
-const baseUrl = 'https://freestream.vercel.app';
+const baseUrl = 'https://freestreaming.vercel.app';
 const today = new Date().toISOString().split('T')[0];
 
 let sitemap = `<?xml version="1.0" encoding="UTF-8"?>
