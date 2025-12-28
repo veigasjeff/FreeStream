f1f49202d531d4b44ac49f0ed218fab8ebada962
verdsouza <110382877+veigasjeff@users.noreply.github.com>
2025-12-28 21:40:19 +0530

    ok updated  dec28 10pm

--- generate-movies.js
+++ generate-movies.js
@@ -101,8 +101,11 @@ function generateMoviePage(movie) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
-    <title>Watch ${movie.title} (${year}) Online Free | HD Streaming | FreeStream</title>
+    <meta name="mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="robots" content="index,follow,archive,snippet,imageindex">
+
+    <title>Watch ${movie.title} Online Free | FreeStream</title>
     <meta name="description" content="${movie.description.replace(/"/g, '&quot;')}">
     <meta name="keywords" content="${movie.keywords}">
     
